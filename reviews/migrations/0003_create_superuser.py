e79b8171a1671c5327a80fe47876d458e9ad9649
Drew Dylewski <ddylewski@tru.local>
2025-08-17 20:53:52 -0500

    Revert "fix(migrations): Make superuser creation more robust"
    This reverts commit 0cecd144113800cd7f79b7561890d91e32ddb1ad.

--- reviews/migrations/0003_create_superuser.py
+++ reviews/migrations/0003_create_superuser.py
@@ -1,25 +1,16 @@
 from django.db import migrations
 import os
 
-def create_or_update_superuser(apps, schema_editor):
+def create_superuser(apps, schema_editor):
     User = apps.get_model('auth', 'User')
 
     username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
     email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
     password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
-    if not (username and email and password):
-        return
-
-    try:
-        user = User.objects.get(username=username)
-        user.email = email
-        user.set_password(password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
-    except User.DoesNotExist:
-        User.objects.create_superuser(username=username, email=email, password=password)
+    if username and email and password:
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
 
 class Migration(migrations.Migration):
 
@@ -27,6 +18,6 @@ class Migration(migrations.Migration):
         ('reviews', '0002_populate_specialties'),
     ]
 
-        operations = [
-        migrations.RunPython(create_or_update_superuser),
+    operations = [
+        migrations.RunPython(create_superuser),
     ]
